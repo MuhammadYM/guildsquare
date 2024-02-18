8cf8b05db36a6c1abf0030ff0cd675164f1f36be
Muhammad <muhammadalnabhani@gmail.com>
2024-02-18 15:20:42 +0000

    fix staticDir

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -43,7 +43,7 @@ export const Media: CollectionConfig = {
   },
   upload: {
     staticURL: "/media",
-    staticDir: "/media",
+    staticDir: "media",
     imageSizes: [
       {
         name: "thumbnail",
